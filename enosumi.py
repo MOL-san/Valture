9f3dd63bfef40bc4215474b9b745819517c927a1
Mol <96118037+MOL-san@users.noreply.github.com>
2022-02-19 12:08:50 +0000

    エラーの改善
    エラーがなくなり、戦績は10000試合行ってkutsu1に対して44.23%(敗:49.46%, 引き分け:6.31%)とsakuraなどに比べて好戦している。ただrandomに対しても勝率50%を切っている。またsakuraシリーズに対しては勝率が40%を切るなどめっぽう弱いため、sakuraシリーズのランダム性への対策とkutsuに対しての勝率50%以上を目指す。avesに対しての1000試合は時間がかかり過ぎた。

--- enosumi.py
+++ enosumi.py
@@ -18,42 +18,44 @@ def sumire(table=[], player={}, player2={}, name="", table_f=[]):
         if i == table_f[-1]:
             break
         order.remove(i)
-    # table_fは今までに出た札を取得するためにvalture.pyに追加
 
+    d = np.array(order)
+    arr = np.abs(d)
+    order_list = list(arr)
+    order_list.sort()
 
     for a in player["card"]:
         exp_1=[]
         for b in player2["card"]:
             exp_1.append(abs(a - b))
         exp_2.append(sum(exp_1))
-    #期待値てきなのを求める(指針としては負けるならなるべく相手との差が大きく、勝つなら相手との差が小さくなるようにする)
-    
-    d = np.array(order)
-    e = np.abs(d)
-    order_list = e.tolist()
-    order_list.sort()
-    #orderを絶対値を取って昇順に並べる(うまくいってない)
- 
+
 
     s1 = sum(table)
-    #table pointを求める
-    if s1 < 11:
+    if s1 <= 11:
         s2 = abs(s1)
         if order_list[0] > s2:
             order_list.insert(0, s2)
         elif order_list[-1] < s2:
             order_list.append(s2)
         else:
-            for i in range(0, len(order_list)):
+            for i in range(0, len(order_list)-1):
                 if order_list[i] <= s2 < order_list[i + 1]:
                   order_list.insert(i+1, s2)
                   break
         o = order_list.index(s2)
     else:
-        o = len(order_list) - 1
-    #場のポイントが何枚で残りの札の中で何番目に重要かを求める
+        o = -1
+
     
-    list1 = list(rankdata(np.array(exp_2)))
-    x = list1.index(o)
+    list1 = list(rankdata(np.array(exp_2), method = "min"))
+    if o in list1:
+        x=list1.index(o)
+    elif o-1 in list1:
+        x=list1.index(o-1)
+    elif o+1 in list1:
+        x = list1.index(o+1)
+    else:
+        x=list1.index(max(list1))
         
     return player["card"][x]
